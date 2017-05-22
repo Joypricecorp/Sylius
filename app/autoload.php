1739a8494fca2cde11768f4fda260dff240f943c
liverbool <nukboon@gmail.com>
2017-05-22 17:16:43 +0700

    c update

--- app/autoload.php
+++ app/autoload.php
@@ -8,5 +8,4 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
-still can't use this version pls. use vcare-v21
 return $loader;
